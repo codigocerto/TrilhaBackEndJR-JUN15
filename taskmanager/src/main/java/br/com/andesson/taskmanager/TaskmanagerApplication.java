b50e6e5e0ac50d3bbce22bc6187a02f6ac67c5e9
Andesson Reis <andessonreys@gmail.com>
2024-06-18 01:14:45 -0300

    🎉 chore: initial commit with Spring Boot project setup

---
+++ taskmanager/src/main/java/br/com/andesson/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.andesson.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+}
---
+++ taskmanager/src/test/java/br/com/andesson/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andesson.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
