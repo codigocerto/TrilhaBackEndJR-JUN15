e69e4159f003997c563aaa01560db4a6f14da252
Andesson Reis <andessonreys@gmail.com>
2024-06-18 19:33:58 -0300

    feat(repository): add findUserByUsername method to UserRepository

--- taskmanager/src/main/java/br/com/andesson/taskmanager/domain/user/repository/UserRepository.java
+++ taskmanager/src/main/java/br/com/andesson/taskmanager/domain/user/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package br.com.andesson.taskmanager.domain.user.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,9 @@ import br.com.andesson.taskmanager.domain.user.model.User;
  */
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+  
+    @Query("select u from User u where u.username = ?1")
+    Optional<User> findUserByUsername(String username);
+    
+
 }
