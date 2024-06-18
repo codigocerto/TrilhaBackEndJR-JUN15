ccb21da87059226cc80da0fa2d6b4bdb7fb6d67c
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-06-18 19:20:05 -0300

    feat: add findAll users

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { UsersServices } from "../services/usersServices";
+import { User } from "../models/userModel";
 
 class UsersController {
   private usersServices: UsersServices;
   constructor() {
     this.usersServices = new UsersServices();
     this.create = this.create.bind(this);
+    this.findAll = this.findAll.bind(this);
   }
 
   async create(request: Request, response: Response, next: NextFunction) {
@@ -15,6 +17,21 @@ class UsersController {
       const result = await this.usersServices.create({ name, email, password });
 
       return response.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async findAll(request: Request, response: Response, next: NextFunction) {
+    try {
+      const users = await this.usersServices.findAll();
+
+      const usersWithoutPassword = users.map((user: User) => {
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+      });
+
+      return response.status(200).json(usersWithoutPassword);
     } catch (error) {
       next(error);
     }
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -3,5 +3,7 @@ import { UsersController } from "../controllers/usersController";
 
 export const userRoutes = (router: Router) => {
   const usersController = new UsersController();
+
   router.post("/users", usersController.create);
+  router.get("/users", usersController.findAll);
 };
