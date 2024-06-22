fda6b2e56f89fd34ce647ff4df1cbf68feea613e
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-06-22 17:48:41 -0300

    refactor: refactored the update user method

--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -2,7 +2,6 @@ import * as bcrypt from "bcrypt";
 import { User, UserUpdate } from "../models/userModel";
 import { UsersRepository } from "../repositories/usersRepository";
 import jwt from "jsonwebtoken";
-import { UpdateValidation } from "../enums/updateValidationEnum";
 
 class UsersServices {
   private usersRepository: UsersRepository;
@@ -70,39 +69,46 @@ class UsersServices {
   ) {
     const userToUpdate = await this.usersRepository.findById(id);
 
-    const validationType = await this.getUpdateValidationType(userToUpdate, {
-      name,
-      email,
-      oldPassword,
-      newPassword,
-    });
-
-    switch (validationType) {
-      case UpdateValidation.USER_NOT_FOUND:
-        throw new Error("User not found");
-
-      case UpdateValidation.MISSING_NEW_PASSWORD:
-      case UpdateValidation.MISSING_OLD_PASSWORD:
-        throw new Error("Problem updating password");
-
-      case UpdateValidation.INVALID_PASSWORD:
-        throw new Error("Invalid password");
-
-      case UpdateValidation.EMPTY_NAME:
-        throw new Error("Name cannot be empty");
-
-      case UpdateValidation.INVALID_EMAIL:
-        throw new Error("Invalid email");
-
-      case UpdateValidation.VALID:
-        if (oldPassword && newPassword) {
-          const hashPassword = await bcrypt.hash(newPassword, 10);
-          await this.usersRepository.updatePassword(hashPassword, id);
-        }
-
-        const updatedUser = await this.usersRepository.update(id, name, email);
-        return updatedUser;
+    if (!userToUpdate) {
+      throw new Error("User not found");
     }
+
+    if (email && !this.isValidEmail(email)) {
+      throw new Error("Invalid email");
+    }
+
+    if (name === undefined || name === "") {
+      throw new Error("Name cannot be empty");
+    }
+
+    const isUpdatingPassword =
+      oldPassword !== undefined || newPassword !== undefined;
+
+    if (isUpdatingPassword) {
+      if (oldPassword === undefined || oldPassword === "") {
+        throw new Error("Old password is required to update password");
+      }
+
+      if (newPassword === undefined || newPassword === "") {
+        throw new Error("New password is required to update password");
+      }
+
+      if (!(await bcrypt.compare(oldPassword, userToUpdate.password))) {
+        throw new Error("Invalid password");
+      }
+
+      if (!this.isValidPassword(newPassword)) {
+        throw new Error(
+          "Invalid new password. Minimum 8 characters with 1 uppercase letter, 1 lowercase letter and 1 special character"
+        );
+      }
+
+      const hashPassword = await bcrypt.hash(newPassword, 10);
+      await this.usersRepository.updatePassword(hashPassword, id);
+    }
+
+    const updatedUser = await this.usersRepository.update(id, name, email);
+    return updatedUser;
   }
 
   async delete(id: string): Promise<void> {
@@ -116,53 +122,13 @@ class UsersServices {
   }
 
   private isValidEmail(email: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+    const emailTest = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailTest.test(email);
   }
 
   private isValidPassword(password: string): boolean {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/;
-    return passwordRegex.test(password);
-  }
-
-  private async getUpdateValidationType(
-    userToUpdate: User | null,
-    { name, email, oldPassword, newPassword }: UserUpdate
-  ): Promise<UpdateValidation> {
-    if (!userToUpdate) {
-      return UpdateValidation.USER_NOT_FOUND;
-    }
-
-    const isUpdatingPassword =
-      oldPassword !== undefined || newPassword !== undefined;
-    const isOldPasswordMissing =
-      oldPassword === undefined || oldPassword === "";
-    const isNewPasswordMissing =
-      newPassword === undefined || newPassword === "";
-
-    if (isUpdatingPassword && (isOldPasswordMissing || isNewPasswordMissing)) {
-      return UpdateValidation.MISSING_NEW_PASSWORD;
-    }
-
-    if (isUpdatingPassword && !isOldPasswordMissing && !isNewPasswordMissing) {
-      const passwordMatch = await bcrypt.compare(
-        oldPassword,
-        userToUpdate.password
-      );
-      if (!passwordMatch) {
-        return UpdateValidation.INVALID_PASSWORD;
-      }
-    }
-
-    if (name !== undefined && name.trim() === "") {
-      return UpdateValidation.EMPTY_NAME;
-    }
-
-    if (email !== undefined && !this.isValidEmail(email)) {
-      return UpdateValidation.INVALID_EMAIL;
-    }
-
-    return UpdateValidation.VALID;
+    const passwordTest = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/;
+    return passwordTest.test(password);
   }
 }
 
