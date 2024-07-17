f3ba44e53db4de1f97ca05f2187e2792ad300700
Matheus <matheusgmello1@gmail.com>
2024-07-17 19:21:44 -0300

    feat: endpoints do usuario

---
+++ src/main/java/dev/matheus/task/controllers/UsuarioController.java
@@ -0,0 +1,35 @@
+package dev.matheus.task.controllers;
+
+import dev.matheus.task.domain.dtos.UsuarioDTO;
+import dev.matheus.task.domain.services.UsuarioService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Users", description = "Endpoints para manipulação de usuários")
+@RequestMapping("/users")
+public class UsuarioController {
+    private final UsuarioService service;
+
+    @GetMapping("/")
+    public ResponseEntity<List<UsuarioDTO>> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> update(@PathVariable Long id, @RequestBody UsuarioDTO usuarioDTO){
+        return ResponseEntity.ok().body(service.update(id, usuarioDTO));
+    }
+}
