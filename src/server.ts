5cc3b3d573e384bfe3f04479a0b937da96c1ad7f
Fabiano Albuquerque <fabiano_albuquerque@Fabianos-MacBook-Pro.local>
2024-07-18 18:11:21 -0300

    general(codigo-certo-api): adjust confi in server file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import { app } from "./app";
 import { appConfig } from "./config/app.config";
 
-app.listen(appConfig.port, () =>
-  console.log(`Server is Running on PORT: ${appConfig.port}`)
+app.listen(
+  {
+    port: appConfig.port,
+    host: "RENDER" in process.env ? "0.0.0.0" : "localhost",
+  },
+  () => {
+    console.log(`Server is running on port ${appConfig.port}`);
+  }
 );
