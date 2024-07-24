2f60cd1d356aa7df7850335c8a0e62569f68b823
Jeferson Santos <jefersonsann@hotmail.com>
2024-07-24 00:12:03 -0300

    docs: Documentação das rotas com swagger path /docs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { json } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { routes } from './routes';
+import swaggerSchema from './swagger.json';
 
 const PORT = process.env.PORT || 3333;
 const HOST = process.env.HOST || '0.0.0.0';
@@ -8,6 +10,7 @@ const app = express();
 app.use(json());
 
 // Rotas
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSchema));
 app.use(routes);
 
 app.listen(PORT, () => {
