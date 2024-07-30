9a5de1dbddc7a3a3fc5625f80b4c32eb89f87ba9
Matheus <matheusgmello1@gmail.com>
2024-07-30 12:55:47 -0300

    feat: adicionar tratamento de excecao para autenticacao invalida

--- src/main/java/dev/matheus/task/controllers/AuthenticationController.java
+++ src/main/java/dev/matheus/task/controllers/AuthenticationController.java
@@ -5,8 +5,13 @@ import dev.matheus.task.domain.dtos.UsuarioLoginDTO;
 import dev.matheus.task.domain.services.AuthenticationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +25,22 @@ public class AuthenticationController {
     private final AuthenticationService service;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody UsuarioLoginDTO user){
-        return ResponseEntity.ok().body(service.login(user));
+    public ResponseEntity<?> login(@RequestBody UsuarioLoginDTO user) {
+        try {
+            LoginResponseDTO loginResponse = service.login(user);
+            return ResponseEntity.ok().body(loginResponse);
+        } catch (AuthenticationException e) {
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Credenciais inválidas. Por favor, tente novamente.");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Void> register(@RequestBody UsuarioLoginDTO user){
+    public ResponseEntity<Map<String, String>> register(@RequestBody UsuarioLoginDTO user) {
         service.register(user);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Usuário criado com sucesso");
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
