52f068a3e586e1532af069173923a54d84e544dc
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-08-13 00:46:40 -0300

    chore: add validation pipes

--- api_task_manager/src/main.ts
+++ api_task_manager/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+  console.log('Started at port 3000');
 }
 bootstrap();
