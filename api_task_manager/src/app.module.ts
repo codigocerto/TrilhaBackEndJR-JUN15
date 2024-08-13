abf0ff9122ef086431a30e146e8bdda86c980b3c
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-08-13 00:45:24 -0300

    feat: upd app.module with typeorm and tasks module

--- api_task_manager/src/app.module.ts
+++ api_task_manager/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
